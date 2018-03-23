2a0f083720411ad07dc4217dc9491ddb7a03a509
Eddie Melendez <exmelendez@gmail.com>
2018-03-23 00:00:28 -0400

    added Person fields/methods

--- src/library/users/Person.java
+++ src/library/users/Person.java
@@ -1,4 +1,18 @@
 package library.users;
 
 public class Person {
+    private String name, email, userType;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    //todo Figure out how to read values from a DB
+    public void checkDuplicateEmails(){
+
+    }
 }
